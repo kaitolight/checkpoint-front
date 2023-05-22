6c892c73c1587736bb4ada2847f107af98c297fa
Alexandre Pillias <alexandre.pillias@gmail.com>
2023-05-22 17:29:37 +0200

    countries fetched + name of each country displayed

--- src/interfaces/interfaces.tsx
+++ src/interfaces/interfaces.tsx
@@ -2,3 +2,9 @@ export interface ContinentsInterface {
   code: string;
   name: string;
 }
+
+export interface CountryInterface {
+  code: string;
+  name: string;
+  emoji: string;
+}
--- src/pages/Countries.tsx
+++ src/pages/Countries.tsx
@@ -1,7 +1,42 @@
+import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import axios from "axios";
+import { CountryInterface } from "../interfaces/interfaces";
 
 export default function Countries() {
+  const [countries, setCountries] = useState([]);
   const location = useLocation();
-  console.log(location.state);
-  return <div>Countries</div>;
+  const countryCode = location.state.continent.code;
+  useEffect(() => {
+    axios({
+      url: "https://countries.nausicaa.wilders.dev/",
+      method: "post",
+      data: {
+        query: `
+        query getContinent($code: ID!) {
+            continent(code: $code) {
+              code
+              name
+              countries {
+                code
+                name
+                emoji
+              }
+            }
+          }`,
+        variables: { code: countryCode },
+      },
+      //
+    }).then((result) => {
+      setCountries(result.data.data.continent.countries);
+    });
+  }, [countryCode]);
+  return (
+    <>
+      <h1>Countries</h1>
+      {countries.map((country: CountryInterface) => {
+        return <p>{country.name}</p>;
+      })}
+    </>
+  );
 }
